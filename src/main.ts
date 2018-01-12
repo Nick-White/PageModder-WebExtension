029d4e3a418ad2511a29bb41c03970b47cc4a742
Nicolae Albu <nicolae_albu2004@yahoo.com>
2018-01-12 23:56:40 +0200

    #1 Example of injecting non-sandboxed JavaScript into the page
    Execute script in the appropriate frame

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import Tab = browser.tabs.Tab; 
 
-browser.webNavigation.onCommitted.addListener((details: {tabId: number, url: string}): void => {
+browser.webNavigation.onCommitted.addListener((details: {tabId: number, frameId: number, url: string}): void => {
     if (details.url === "file:///D:/Programare/Proiecte/PageModder-WebExtension/dist/options/options.html") {
         browser.tabs.executeScript(details.tabId, {
+            frameId: details.frameId,
             /*
             executeScript returns whatever the last statement inside the code returns, so I'll just add a null,
             otherwise it might try to return something that is not serializable (function etc.), and an error will be thrown.
             */
             code: "window.eval('window.open = function() {alert(\"disabled\")};null;')",
-            runAt: "document_idle"
+            runAt: "document_start"
         });
     }
 });
